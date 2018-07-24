adf249485d92f5aefec8eca2ab5de3ea336a0adb
Richard Darst <rkd@zgib.net>
2018-07-24 20:03:45 +0300

    Add OFFSET and time range selection support

--- niimpy/__init__.py
+++ niimpy/__init__.py
@@ -1,10 +1,12 @@
 
 from __future__ import print_function, division
 
+import datetime
 import os
 import sqlite3
 import sys
 
+import dateutil.parser
 import pandas as pd
 
 # https://sqlite.org/contrib/download/extension-functions.c?get=25
@@ -50,6 +52,20 @@ class Data1(object):
             return "AND user=:user"
         raise ValueError("Specify user or user=niimpy.ALL for all users")
 
+    def _sql_where_daterange(self, start, end):
+        """SQL WHERE start <= time AND time < end."""
+        where = ""
+        def to_timestamp(x):
+            if isinstance(x, int): return x
+            if isinstance(x, str): return dateutil.parser.parse(x).timestamp()
+            if isinstance(x, datetime.datetime): return x.timestamp()
+            raise ValueError("Unknown timestamp format: {}".format(x))
+        if start:
+            where += " AND %s <= time "%(to_timestamp(start))
+        if end:
+            where += " AND time < %s "%(to_timestamp(end))
+        return where
+
     def _sql_select_user(self, user):
         """SQL SELECT 'user, ' line, if needed."""
         if self._singleuser:
@@ -60,11 +76,18 @@ class Data1(object):
             return ""
         raise ValueError("Specify user or user=niimpy.ALL for all users")
 
-    def _sql_limit(self, limit):
+    def _sql_limit(self, limit, offset=None):
         """SQL LIMIT line, if needed."""
+        if offset is not None:
+            if limit is None:  return "LIMIT -1 OFFSET %s"%(int(offset), )
+            else:  return "LIMIT %s OFFSET %s"%(int(limit), int(offset))
         if limit is None: return ""
-        assert isinstance(limit, int)
-        return 'LIMIT %s'%limit
+        return 'LIMIT %s'%int(limit)
+
+    def _sql_order_by(self, order=False):
+        if not order: return ""
+        return "ORDER BY time"
+
 
     def _sql_group_by_user(self):
         """SQL LIMIT line, if needed."""
@@ -72,10 +95,12 @@ class Data1(object):
             return ""
         return "GROUP BY user"
 
-    def _sql(self, user, limit=None):
+    def _sql(self, user, limit=None, offset=None, order=False, start=None, end=None):
         return dict(select_user=self._sql_select_user(user),
                     where_user=self._sql_where_user(user),
-                    limit=self._sql_limit(limit),
+                    where_daterange=self._sql_where_daterange(start, end),
+                    limit=self._sql_limit(limit, offset=None),
+                    order=self._sql_order_by(order),
                     group_by_user=self._sql_group_by_user(),
                    )
 
@@ -126,38 +151,38 @@ class Data1(object):
                 user_stats[table_][user] = count
         return user_stats
 
-    def first(self, table, user, aggregate="min"):
+    def first(self, table, user, start=None, end=None, _aggregate="min"):
         """Return earliest data point.
 
         Return None if there is no data."""
         df = pd.read_sql("""SELECT {select_user} {aggregate}(time) AS {result_column_name}
                               FROM "{table}"
-                              WHERE 1 {where_user}
+                              WHERE 1 {where_user} {where_daterange}
                               {group_by_user}
                         """.format(table=table,
-                                   aggregate=aggregate,
-                                   result_column_name='time' if aggregate!='count' else 'count',
-                                   **self._sql(user=user, limit=None)),
+                                   aggregate=_aggregate,
+                                   result_column_name='time' if _aggregate!='count' else 'count',
+                                   **self._sql(user=user, limit=None, start=start, end=end)),
                         self.conn, params={'user':user, })
         if df.empty:
             return None
         if 'time' in df:
             df['datetime'] = pd.to_datetime(df['time'], unit='s')
         return df
-    def last(self, table, user):
+    def last(self, *args, **kwargs):
         """Return the latest timestamp.
 
         See the "first" for more information."""
-        return self.first(table, user, aggregate="max")
-    def count(self, table, user):
+        return self.first(*args, _aggregate="max", **kwargs)
+    def count(self, *args, **kwargs):
         """Return the number of rows
 
         See the "first" for more information."""
-        return self.first(table, user, aggregate="count")
+        return self.first(*args, _aggregate="count", **kwargs)
 
 
 
-    def quality(self, table, user, limit=None):
+    def quality(self, table, user, limit=None, start=None, end=None):
         n_intervals = 5
         interval_width = 60/n_intervals
         df = pd.read_sql("""SELECT {select_user} day, hour, interval,
@@ -169,17 +194,17 @@ class Data1(object):
                                 CAST(strftime('%M', time, 'unixepoch', 'localtime')/:interval_width AS INTEGER) AS interval,
                                 count(*) as bin_count
                                FROM "{table}"
-                               WHERE 1 {where_user}
+                               WHERE 1 {where_user} {where_daterange}
                                GROUP BY day, hour, interval)
                              GROUP BY day, hour
                              {limit}
                         """.format(table=table,
-                                   **self._sql(user=user, limit=limit)),
+                                   **self._sql(user=user, limit=limit, start=start, end=end)),
                         self.conn, params={'user':user, 'interval_width':interval_width})
         return df
 
 
-    def hourly(self, table, user, columns='hr', limit=None):
+    def hourly(self, table, user, columns='hr', limit=None, start=None, end=None):
         if isinstance(columns, str):
             columns = [columns]
         column_selector = ",\n".join("    avg({0}) AS {0}_mean, stdev({0}) AS {0}_std, count({0}) AS {0}_count".format(c) for c in columns)
@@ -191,35 +216,37 @@ class Data1(object):
                                 CAST(strftime('%H', time, 'unixepoch', 'localtime') AS INTEGER) AS hour,
                                 count(*) as count {column_selector}
                             FROM "{table}"
-                               WHERE 1 {where_user}
+                               WHERE 1 {where_user} {where_daterange}
                             GROUP BY day, hour
                             {limit}
                          """.format(table=table, column_selector=column_selector,
-                                   **self._sql(user=user, limit=limit)),
+                                   **self._sql(user=user, limit=limit, start=start, end=end)),
                          self.conn, params={'user':user})
         return df
 
 
-    def raw(self, table, user, limit=None):
+    def raw(self, table, user, limit=None, offset=None, start=None, end=None):
         df = pd.read_sql("""SELECT
                                 *
                             FROM "{table}"
-                               WHERE 1 {where_user}
+                               WHERE 1 {where_user} {where_daterange}
                             {limit}
                         """.format(table=table,
-                                   select_user=self._sql_select_user(user),  where_user=self._sql_where_user(user),
-                                   limit=self._sql_limit(limit)),
+                                   **self._sql(user=user, limit=limit, start=start, end=end)
+                                   #select_user=self._sql_select_user(user),  where_user=self._sql_where_user(user),
+                                   #limit=self._sql_limit(limit)
+                                   ),
                         self.conn, params={'user':user})
         if 'time' in df:
             df['datetime'] = pd.to_datetime(df['time'], unit='s')
         return df
 
-    def get_survey_score(self, table, user, survey, limit=None):
+    def get_survey_score(self, table, user, survey, limit=None, start=None, end=None):
         """Get the survey results, summing scores.
 
         survey: The servey prefix in the 'id' column, e.g. 'PHQ9'.  An '_' is appended.
         """
-        questions = self.raw(table=table, user=user, limit=limit)
+        questions = self.raw(table=table, user=user, limit=limit, start=start, end=end)
         answers = questions[questions['id'].str.startswith(survey+'_')]
         answers['answer'] = pd.to_numeric(answers['answer'])
         survey_score = answers.groupby(['user','time'])['answer'].sum(skipna=False)
