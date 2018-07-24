6fc0f26f5975af0ad9b0809917aa87340244ad89
Richard Darst <rkd@zgib.net>
2018-07-24 18:14:27 +0300

    Add support for single-user databases

--- niimpy/__init__.py
+++ niimpy/__init__.py
@@ -35,46 +35,71 @@ class Data1(object):
             print("SQLite3 extension module not available, some functions will not work.", file=sys.stderr)
             print("Future niimpy versions will improve this.", file=sys.stderr)
             print("({0})".format(SQLITE3_EXTENSIONS_FILENAME), file=sys.stderr)
-
+        self._singleuser = self._is_single_user()
 
     def tables(self):
         return {x[0] for x in self.conn.execute('SELECT name FROM sqlite_master WHERE type="table"') if x[0]!='errors'}
 
-    @staticmethod
-    def _sql_where_user(user):
+    def _sql_where_user(self, user):
         """SQL WHERE user={user} line, if needed."""
+        if self._singleuser:
+            return ""
         if user is ALL:
             return ""
         elif user:
             return "AND user=:user"
         raise ValueError("Specify user or user=niimpy.ALL for all users")
 
-    @staticmethod
-    def _sql_select_user(user):
+    def _sql_select_user(self, user):
         """SQL SELECT 'user, ' line, if needed."""
+        if self._singleuser:
+            return ""
         if user is ALL:
             return "user, "
         elif user:
             return ""
         raise ValueError("Specify user or user=niimpy.ALL for all users")
 
-    @staticmethod
-    def _sql_limit(limit):
+    def _sql_limit(self, limit):
         """SQL LIMIT line, if needed."""
         if limit is None: return ""
         assert isinstance(limit, int)
         return 'LIMIT %s'%limit
 
-    @classmethod
-    def _sql(cls, user, limit):
-        return dict(select_user=cls._sql_select_user(user),
-                    where_user=cls._sql_where_user(user),
-                    limit=cls._sql_limit(limit),
+    def _sql_group_by_user(self):
+        """SQL LIMIT line, if needed."""
+        if self._singleuser:
+            return ""
+        return "GROUP BY user"
+
+    def _sql(self, user, limit=None):
+        return dict(select_user=self._sql_select_user(user),
+                    where_user=self._sql_where_user(user),
+                    limit=self._sql_limit(limit),
+                    group_by_user=self._sql_group_by_user(),
                    )
+
+    def _is_single_user(self):
+        """Detect if this is a single-user database
+
+        Currently this is run at the start and set per-database, not per-table.
+        """
+        tables = self.tables()
+        for table in tables:
+            if table == 'errors': continue
+            has_user_column = self.conn.execute("SELECT name FROM pragma_table_info(\"AwareScreen\") WHERE name='user'").fetchall()
+            if not has_user_column:
+                self._singleuser = True
+                print("Detected single-user database", file=sys.stderr)
+                return True
+        return False
+
 
 
     def users(self, table=None):
         """Set of all users in all tables"""
+        if self._singleuser:
+            return None
         if table is not None:  tables = [table]
         else:                  tables = self.tables()
         users = set()
@@ -84,10 +109,19 @@ class Data1(object):
 
     def user_table_counts(self):
         """More detailed user stats"""
+        if self._singleuser:
+            print(self.tables())
+            user_stats = pd.DataFrame(index=sorted(self.tables()), columns=("count",))
+            print(user_stats)
+            cur = self.conn.cursor()
+            for table_ in self.tables():
+                for count, in cur.execute('SELECT count(*) FROM "{table}"'.format(table=table_, **self._sql(user=ALL))):
+                    user_stats['count'][table_] = count
+            return user_stats
         user_stats = pd.DataFrame(index=sorted(self.users()), columns=sorted(self.tables()))
         cur = self.conn.cursor()
         for table_ in self.tables():
-            for user, count in cur.execute('SELECT user, count(*) FROM "{table}" GROUP BY user'.format(table=table_)):
+            for user, count in cur.execute('SELECT {select_user} count(*) FROM "{table}" GROUP BY user'.format(table=table_, **self._sql(user=ALL))):
                 if user is None: continue
                 user_stats[table_][user] = count
         return user_stats
@@ -99,7 +133,7 @@ class Data1(object):
         df = pd.read_sql("""SELECT {select_user} {max_or_min}(time) AS time
                               FROM "{table}"
                               WHERE 1 {where_user}
-                              GROUP BY user
+                              {group_by_user}
                         """.format(table=table,
                                    max_or_min="max" if last else "min",
                                    **self._sql(user=user, limit=None)),
