df4ddf547628581620848cfe97036152d3f61261
Richard Darst <rkd@zgib.net>
2020-01-16 08:33:23 +0200

    Add multi-user sample data

--- niimpy/sampledata.py
+++ niimpy/sampledata.py
@@ -5,4 +5,7 @@ Sample data of different types
 import os
 
 _dirname = os.path.join(os.path.dirname(__file__), 'sampledata')
+# A simple single-user dataset
 DATA = os.path.join(_dirname, 'singleuser.sqlite3')
+# A simple multi-user dataset: one user, two devices, AwareScreen and AwareBattery
+MULTIUSER = os.path.join(_dirname, 'multiuser.sqlite3')
