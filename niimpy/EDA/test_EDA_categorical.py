db43841bf480451ac0eaf099bc431f4f487e4435
Arsi Ikäheimonen <61492903+ArgonSilicon@users.noreply.github.com>
2022-02-07 13:58:56 +0200

    Updated EDA_categorical tests

--- niimpy/EDA/test_EDA_categorical.py
+++ niimpy/EDA/test_EDA_categorical.py
@@ -42,149 +42,25 @@ def test_questionnaire_summary():
                                                 width=900, height=900)
     assert (type(fig) == plotly.graph_objs._figure.Figure)
     
-# 5) def question_by_group(df, question, id_column = 'id', answer_column = 'answer', group='group'):
+def test_question_by_group():
+    df = setup_dataframe.create_categorical_dataframe()
+    grouped = EDA_categorical.question_by_group(df, question = 'id_1', id_column = 'id',\
+                                                answer_column = 'answer',   group='group')
+    #TODO! test contents of groupby object
+    assert (type(grouped) == pd.core.frame.DataFrame)
+    
+def test_plot_grouped_categories():
+    df = setup_dataframe.create_categorical_dataframe()
+    grouped = EDA_categorical.question_by_group(df, question = 'id_1', id_column = 'id',\
+                                                answer_column = 'answer',   group='group')
+    fig = EDA_categorical.plot_grouped_categories(grouped, group='group', title=None, xlabel=None, \
+                                                  ylabel=None, width=900, height=900)
+    assert (type(fig) == plotly.graph_objs._figure.Figure)
+    
+def test_questionnaire_grouped_summary():
+    df = setup_dataframe.create_categorical_dataframe()
+    fig = EDA_categorical.questionnaire_grouped_summary(df, question='id_1', id_column ='id',\
+                                                        answer_column='answer',group = 'group',title=None,\
+                                                        xlabel=None,) 
 
-# 6) def plot_grouped_categories(df, group, title=None, xlabel=None, ylabel=None, width=900, height=900):
-
-# 7) def questionnaire_grouped_summary(
-#    df, question, id_column ='id', answer_column = 'answer', group = 'group', title=None, xlabel=None, 
-# ylabel=None, width=900, height=900
-#):
-
-# Read some mock dataframe and test the functions then
-'''
-class TestEDAcategorical(object):
-
-    def test_EDA_questionnaire_summary(self):
-        """
-        Test EDA questionnaire summary functions. The test fails when arguments:
-            - data is not a pandas dataframe
-            - columns a is not a string or a list
-            - title is not a string
-            - xlabel is not a string
-            - ylabel is not a string
-            - resample is not a string or boolean
-            - interpolate is not a boolean
-            - window is not an integer
-            - reset_index is not a boolean
-            - by in not a string or a boolean
-
-        Returns
-        -------
-        None.
-
-        """
-        df = setup_dataframe.create_categorical_dataframe()
-
-        # Store information about raised ValueError in exc_info
-        with pytest.raises(AssertionError) as exc_info:
-            EDA_categorical.questionnaire_summary(df.to_numpy(),
-                                                  question = 'question',
-                                                  column = 'answer',
-                                                  title = 'title',
-                                                  xlabel = 'xlabel',
-                                                  ylabel = 'ylabel',
-                                                  user = None)
-
-        expected_error_msg = "df is not a pandas dataframe."
-        # Check if the raised ValueError contains the correct message
-        assert exc_info.match(expected_error_msg)
-
-        # Store information about raised ValueError in exc_info
-        with pytest.raises(AssertionError) as exc_info:
-            EDA_categorical.questionnaire_summary(df,
-                                                  question = 1,
-                                                  column = 'answer',
-                                                  title = 'title',
-                                                  xlabel = 'xlabel',
-                                                  ylabel = 'ylabel',
-                                                  user = None)
-
-        expected_error_msg = "question is not a string."
-        # Check if the raised ValueError contains the correct message
-        assert exc_info.match(expected_error_msg)
-
-        # Store information about raised ValueError in exc_info
-        with pytest.raises(AssertionError) as exc_info:
-            EDA_categorical.questionnaire_summary(df,
-                                                  question = 'question',
-                                                  column = 1,
-                                                  title = 'title',
-                                                  xlabel = 'xlabel',
-                                                  ylabel = 'ylabel',
-                                                  user = None)
-
-        expected_error_msg = "column is not a string."
-        # Check if the raised ValueError contains the correct message
-        assert exc_info.match(expected_error_msg)
-
-        # Store information about raised ValueError in exc_info
-        with pytest.raises(AssertionError) as exc_info:
-            EDA_categorical.questionnaire_summary(df,
-                                                  question = 'question',
-                                                  column = 'answer',
-                                                  title = 1,
-                                                  xlabel = 'xlabel',
-                                                  ylabel = 'ylabel',
-                                                  user = None)
-
-        expected_error_msg = "title is not a string."
-        # Check if the raised ValueError contains the correct message
-        assert exc_info.match(expected_error_msg)
-
-        # Store information about raised ValueError in exc_info
-        with pytest.raises(AssertionError) as exc_info:
-            EDA_categorical.questionnaire_summary(df,
-                                                  question = 'question',
-                                                  column = 'answer',
-                                                  title = 'title',
-                                                  xlabel = 1,
-                                                  ylabel = 'ylabel',
-                                                  user = None)
-
-        expected_error_msg = "xlabel is not a string."
-        # Check if the raised ValueError contains the correct message
-        assert exc_info.match(expected_error_msg)
-
-        # Store information about raised ValueError in exc_info
-        with pytest.raises(AssertionError) as exc_info:
-            EDA_categorical.questionnaire_summary(df,
-                                                  question = 'question',
-                                                  column = 'answer',
-                                                  title = 'title',
-                                                  xlabel = 'xlabel',
-                                                  ylabel = 1,
-                                                  user = None)
-
-        expected_error_msg = "ylabel is not a string."
-        # Check if the raised ValueError contains the correct message
-        assert exc_info.match(expected_error_msg)
-
-        # Store information about raised ValueError in exc_info
-        with pytest.raises(AssertionError) as exc_info:
-            EDA_categorical.questionnaire_summary(df,
-                                                  question = 'question',
-                                                  column = 'answer',
-                                                  title = 'title',
-                                                  xlabel = 'xlabel',
-                                                  ylabel = 'ylabel',
-                                                  user = 1)
-
-        expected_error_msg = "user is not a boolean or string."
-        # Check if the raised ValueError contains the correct message
-        assert exc_info.match(expected_error_msg)
-
-        # Store information about raised ValueError in exc_info
-        with pytest.raises(AssertionError) as exc_info:
-            EDA_categorical.questionnaire_grouped_summary(df,
-                                                  question = 'question',
-                                                  column = 'answer',
-                                                  title = 'title',
-                                                  xlabel = 'xlabel',
-                                                  ylabel = 'ylabel',
-                                                  group = 1)
-
-        expected_error_msg = "group is not a boolean or string."
-        # Check if the raised ValueError contains the correct message
-        assert exc_info.match(expected_error_msg)
-'''
+    assert (type(fig) == plotly.graph_objs._figure.Figure)
