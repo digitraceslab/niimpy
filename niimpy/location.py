7ca72d39d464ea8b2ed651dfdbde69f0cfa989fe
Ziaei Bideh Amirmohammad <amirmohammad.ziaeibideh@aalto.fi>
2022-02-07 14:55:04 +0200

    Add speed-related features

--- niimpy/location.py
+++ niimpy/location.py
@@ -1,5 +1,6 @@
 
 import pandas as pd
+import numpy as np
 
 from geopy.distance import geodesic
 
@@ -40,7 +41,6 @@ def bin_location(location,
         lambda x: x.floor(freq=freq, ambiguous=False)
     )
 
-
     original_columns = location.columns
     columns_others = location.columns.drop(columns_to_aggregate)
     columns_to_aggregate.extend(['user', 'time'])
@@ -48,9 +48,12 @@ def bin_location(location,
     location_to_aggregate = location[columns_to_aggregate]
     location_others = location[columns_others]
 
+    grouped = location_to_aggregate.groupby(['user', 'time'])
+    if aggregation == 'median':
+        location_to_aggregate = grouped.median()
+    elif aggregation == 'mean':
+        location_to_aggregate = grouped.mean()
     location_to_aggregate = location_to_aggregate. \
-        groupby(['user', 'time']). \
-        median(). \
         reset_index(level=[0, 1]). \
         set_index('time')
 
@@ -63,12 +66,11 @@ def bin_location(location,
 
     location = pd.concat([location_to_aggregate, location_others], axis=1)
     location = location[original_columns.drop('time')]
-    location = location.drop_duplicates()
     return location
 
 
-def extract_total_distance(location, column_prefix=None):
-    """Calculates the total distance traveled for each user
+def extract_distance_features(location, column_prefix=None):
+    """Calculates features realted distance and speed
 
     Parameters
     ----------
@@ -81,22 +83,47 @@ def extract_total_distance(location, column_prefix=None):
 
     Returns
     -------
-    total_dist : pd.DataFrame
-        Dataframe of computed distances where the index is users and columns
-        are the calculated features.
+    features : pd.DataFrame
+        Dataframe of computed features where the index is users and columns
+        are the the features. Featerus
+            - `dist_total`: total distance traveled
+            - `n_bins`: number of bins with which other features are calculated
+            - `speed_average`: average speed
+            - `speed_variance`: variance in speed
     """
-    def compute_total_distance(df):
-        """Compute total distance for a single user"""
-        dist = 0
+    def compute_distance_features(df):
+        """Compute features for a single user"""
+        total_dist = 0
+        speeds = []
         for i in range(df.shape[0] - 2):
             loc1 = df.iloc[i][['double_latitude', 'double_longitude']]
             loc2 = df.iloc[i + 1][['double_latitude', 'double_longitude']]
-            dist += geodesic(loc1, loc2).meters
+            dist = geodesic(loc1, loc2).meters
+            total_dist += dist
+
+            time_delta = (df.index[i + 1] - df.index[i]).seconds
+            if time_delta > 0:
+                speeds.append(dist / time_delta)
+        speed_average = np.mean(speeds)
+        speed_variance = np.var(speeds)
         row = pd.DataFrame({
-            'total_dist': [dist],
-            'normalized_total_dist': [dist / df.shape[0]]
+            'dist_total': [total_dist],
+            'n_bins': [df.shape[0]],
+            'speed_average': [speed_average],
+            'speed_variance': [speed_variance]
         })
         return row
 
-    total_dist = location.groupby('user').apply(compute_total_distance)
-    return total_dist
+    features = location.groupby('user').apply(compute_distance_features)
+    features = features.reset_index(level=[1], drop=True)
+
+    if column_prefix:
+        new_columns = [
+            '{}_{}'.format(column_prefix, col) for col in features.columns
+        ]
+        features.columns = new_columns
+
+    if 'group' in location:
+        features['group'] = location.groupby('user')['group'].first()
+
+    return features
