a26ae2255f1410969736eea9f7fc99df2d783de8
Your Name <luongnguyenkhoinguyen14@gmail.com>
2022-04-04 16:21:19 +0300

    Test that step summary function works properly

--- niimpy/preprocessing/test_tracker.py
+++ niimpy/preprocessing/test_tracker.py
@@ -1,14 +1,18 @@
 import numpy as np
 import pandas as pd
 
-import niimpy.preprocessing.survey
+import niimpy.preprocessing.tracker as tracker
+from niimpy.preprocessing import sampledata
 
-def test_sum_survey_scores_indexonly():
-    df = df1.copy()
-    df.index.name = None
+def test_step_summary():
+    
+    df = pd.read_csv(sampledata.TEST_STEP_SUMMARY, index_col=0)
+    # Converting the index as date
+    df.index = pd.to_datetime(df.index)
 
-    results = niimpy.preprocessing.survey.sum_survey_scores(df, 'S1')
-
-    assert results.loc['2019-01-01']['score'] == 3
-    assert results.loc['2019-01-02']['score'] == 12
-    assert np.isnan(results.loc['2019-01-03']['score'])
+    summary_df = tracker.step_summary(df, value_col='steps')
+    assert summary_df['max_sum_step'].values[0] == 13025
+    assert summary_df['min_sum_step'].values[0] == 5616
+    assert round(summary_df['avg_sum_step'].values[0], 2) == 8437.38
+    assert round(summary_df['std_sum_step'].values[0], 2) == 3352.35
+    assert summary_df['median_sum_step'].values[0], 2 == 6480.0
