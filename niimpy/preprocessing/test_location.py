87f9a0c91921763907a40c39d23142ae02d44c1f
Ziaei Bideh Amirmohammad <amirmohammad.ziaeibideh@aalto.fi>
2022-05-05 18:58:53 +0300

    Add test for computed locaiton features

--- niimpy/preprocessing/test_location.py
+++ niimpy/preprocessing/test_location.py
@@ -29,3 +29,41 @@ def test_distance_matrix():
     error_percentage = absolute_error[non_diag_mask] / true_dist_matrix[non_diag_mask]
     
     assert (error_percentage < 0.01).all() # error percentage must be below 1%
+    
+
+def test_location_features():
+    
+    # bin data
+    binned_data = niimpy.util.aggregate(data, freq="10T", method_numerical="median"). \
+        reset_index(0). \
+        dropna()
+    
+    assert data.shape[0] >= binned_data.shape[0], "Number of rows should not increase"
+    
+    # extract featuers
+    features = nilo.extract_features_location(binned_data)
+    
+    assert ((features['n_sps'] > 0) & (features['n_sps'] < 100)).all(), "Number of SPs not reasonable"
+    
+    features_u1 = features[features.user == 'gps_u00']
+    assert features_u1['n_sps'] == 19.0
+    assert features_u1['n_static'] == 4080.0
+    assert features_u1['n_moving'] == 151.0
+    assert features_u1['n_rare'] == 171.0
+    assert features_u1['n_home'] == 2148.0
+    assert features_u1['max_dist_home'] == 6392724.06503727
+    assert features_u1['n_transitions'] == 354.0
+    assert features_u1['n_top1'] == 2170.0
+    assert features_u1['n_top2'] == 1207.0
+    assert features_u1['n_top3'] == 171.0
+    assert features_u1['n_top4'] == 165.0
+    assert features_u1['n_top5'] == 63.0
+    assert features_u1['entropy'] == 7.2388908639293845
+    assert features_u1['normalized_entropy'] == 2.458495800099307
+    assert features_u1['dist_total'] == 18633281.86911233
+    assert features_u1['n_bins'] == 4231.0
+    assert features_u1['speed_average'] == 0.2878325907043252
+    assert features_u1['speed_variance'] == 6.31109370517077
+    assert features_u1['speed_max'] == 34.0
+    assert features_u1['variance'] == 183.8299063365458
+    assert features_u1['log_variance'] == 5.214010907983394
