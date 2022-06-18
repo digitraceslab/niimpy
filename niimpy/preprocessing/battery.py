c70b104cb08dbddad5dec53cc39d1bf40061abbb
Your Name <luongnguyenkhoinguyen14@gmail.com>
2022-06-18 15:41:26 +0300

    Reorganize tests into its own folders. Remove preprocess.py

--- niimpy/preprocessing/battery.py
+++ niimpy/preprocessing/battery.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 
 import niimpy
-from niimpy.preprocessing import preprocess
+
 
 def shutdown_info(df):
     """ Returns a DataFrame with the timestamps of when the phone has shutdown.
