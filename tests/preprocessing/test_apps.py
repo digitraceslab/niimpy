d58bcf7044bcc9c0ad0b4831678c994ec9cf0ddc
AnaTomomi <ana.trianahoyos@aalto.fi>
2022-07-25 16:51:08 +0300

    swap lines to allow definition of new functions for application, update the app notebook

--- tests/preprocessing/test_apps.py
+++ tests/preprocessing/test_apps.py
@@ -23,8 +23,8 @@ def test_audio_features():
     assert test.loc["iGyXetHE3S8u", "comm", time]["duration"] == 37
     assert test.loc["iGyXetHE3S8u", "work", time]["duration"] == 6
        
-    features ={"app_count":{"app_column_name":"application_name", "screen_column_name":"screen_status","resample_args":{"rule":"30S"}},
-               "app_duration":{"resample_args":{"rule":"1T"}}}
+    features ={app.app_count:{"app_column_name":"application_name", "screen_column_name":"screen_status","resample_args":{"rule":"30S"}},
+               app.app_duration:{"resample_args":{"rule":"1T"}}}
     test = app.extract_features_app(data, battery, screen, features=features)
     
     time = "2019-08-05 14:03:00+03:00"
