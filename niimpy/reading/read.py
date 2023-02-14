5aabba43712e9088d31a43cf62497a9b9f5cc7af
Rantaharju Jarno <jarno.rantaharju@aalto.fi>
2023-02-14 16:27:22 +0200

    Format and comment the mHealth reader code

--- niimpy/reading/read.py
+++ niimpy/reading/read.py
@@ -245,35 +245,30 @@ def format_mhealth_time_interval(df, prefix):
 
     In the second case, the formatted database will contain two columns:
     start and end.
-    '''
+    '''    
+    # Create any of the result columns thay may not exist
+    for col in ["start", "end"]:
+        if col not in df.columns:
+            df[col] = None
+
     if f'{prefix}.part_of_day' in df.columns:
         df = format_mHealth_part_of_day(df, prefix)
-    
+
     # Construct column names from the prefix
     start_col = f'{prefix}.start_date_time'
     end_col = f'{prefix}.end_date_time'
     duration_value_col = f'{prefix}.duration.value'
     duration_unit_col = f'{prefix}.duration.unit'
-    
-    # The columns might not exist, so we create them manually
-    if "start" not in df.columns:
-        df["start"] = None
-    if "end" not in df.columns:
-        df["end"] = None
 
-    # All the mHealth columns might exist if the formatting in the data is mixed
-    if start_col in df.columns:
-        rows = ~df[start_col].isnull()
-        df.loc[rows, "start"] = pd.to_datetime(df.loc[rows, start_col])
-
-    if end_col in df.columns:
-        rows = ~df[end_col].isnull()
-        df.loc[rows, "end"] = pd.to_datetime(df.loc[rows, end_col])
+    # Format the date-like columns. All the mHealth columns might exist if the formatting in the data is mixed
+    for col, mHealth_col in [("start", start_col), ("end", end_col)]:
+        if col in df.columns:
+            rows = ~df[mHealth_col].isnull()
+            df.loc[rows, col] = pd.to_datetime(df.loc[rows, mHealth_col], utc=True)
 
     if duration_value_col in df.columns:
         # Format duration as DateOffset. We use this below to calculate either start of end
         mHealth_DateOffset = {
-            "ps": "picoseconds",
             "ns": "nanoseconds",
             "us": "microseconds",
             "ms": "milliseconds",
@@ -289,12 +284,12 @@ def format_mhealth_time_interval(df, prefix):
         def mHealth_to_DateOffset(row):
             unit = mHealth_DateOffset[row[duration_unit_col]]
             value = row[duration_value_col]
-            if unit == "picoseconds":
-                # DateOffset doesn't support picoseconds
+
+            if unit == "picoseconds": # DateOffset doesn't support picoseconds
                 unit = "nanoseconds"
                 value *= 0.001
-            dataoffset = pd.tseries.offsets.DateOffset(**{unit: value})
-            return dataoffset
+            
+            return pd.tseries.offsets.DateOffset(**{unit: value})
 
         rows = ~df[duration_value_col].isnull()
         df.loc[rows, duration_value_col] = df.loc[rows].apply(mHealth_to_DateOffset, axis=1)
@@ -311,15 +306,35 @@ def format_mhealth_time_interval(df, prefix):
     # Drop the original columns
     df = df.drop([start_col, end_col, duration_value_col, duration_unit_col], axis=1)
     rows = ~df["start"].isnull()
-    print(df["timestamp"])
     df.loc[rows, "timestamp"] = df.loc[rows, "start"]
-    print(df["timestamp"])
 
     return df
 
 
 
 def read_mhealth_total_sleep_time(data_list):
+    '''
+    Format mHealth total sleep data from json formatted data to a Niimpy
+    compatible DataFrame. The DataFrame contains the columns
+        - total_sleep_time : The total sleep time measurement
+        - total_sleep_time_unit : The unit the measuremnet is expressed in
+        - measurement interval columns
+        - possible descriptive statistics columns
+
+    The measurement interval column are either
+        - start : start time of the measurement interval
+        - end : end time of the measurement interval
+    or
+        - date : the date of the measurement
+        - part_of_day : the time of day the measurement was made
+    
+    The descriptive statistics columns would be
+        - descriptive_statistics : Describes how the measurement is calculated
+        - descriptive_statistics_denomirator : Time interval the above desciption refers to.
+
+    The dataframe is indexed by "timestamp", which is either the "start" or the "date".
+    '''
+    
     df = pd.json_normalize(data_list)
     total_sleep_time_columns = {
         "total_sleep_time.value": "total_sleep_time",
@@ -329,12 +344,32 @@ def read_mhealth_total_sleep_time(data_list):
     df = format_mhealth_time_interval(df, "effective_time_frame.time_interval")
     df = df.rename(columns=total_sleep_time_columns)
 
-    df["timestamp"] = df["start"]
     df.set_index('timestamp', inplace=True)
     return df
 
 
 def read_mhealth_total_sleep_time_from_file(filename):
+    ''' Read mHealth total sleep time from a file and convert into a Niimpy
+    compatible DataFrame using read_mhealth_total_sleep_time. The dataframe
+    contains the columns
+        - total_sleep_time : The total sleep time measurement
+        - total_sleep_time_unit : The unit the measuremnet is expressed in
+        - measurement interval columns
+        - possible descriptive statistics columns
+
+    The measurement interval column are either
+        - start : start time of the measurement interval
+        - end : end time of the measurement interval
+    or
+        - date : the date of the measurement
+        - part_of_day : the time of day the measurement was made
+    
+    The descriptive statistics columns would be
+        - descriptive_statistics : Describes how the measurement is calculated
+        - descriptive_statistics_denomirator : Time interval the above desciption refers to.
+
+    The dataframe is indexed by "timestamp", which is either the "start" or the "date".
+    '''
     with open(filename) as f:
         data = json.load(f)
 
