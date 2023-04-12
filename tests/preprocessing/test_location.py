ad9ead4c0b9ff34126644786f5c2ceee9a0b1e4f
Rantaharju Jarno <jarno.rantaharju@aalto.fi>
2023-04-12 11:43:26 +0300

    Fix location tests

--- tests/preprocessing/test_location.py
+++ tests/preprocessing/test_location.py
@@ -33,38 +33,33 @@ def test_distance_matrix():
     
 
 def test_location_features():
-    
-    # bin data
-    binned_data = niimpy.util.aggregate(data, freq="10T", method_numerical="median"). \
-        reset_index(0). \
-        dropna()
-    
-    assert data.shape[0] >= binned_data.shape[0], "Number of rows should not increase"
+
+    assert data.shape[0] >= data.shape[0], "Number of rows should not increase"
     
     # extract featuers
-    features = nilo.extract_features_location(binned_data)
+    features = nilo.extract_features_location(data)
     
     assert ((features['n_sps'] > 0) & (features['n_sps'] < 100)).all(), "Number of SPs not reasonable"
-    
-    features_u1 = features.loc['gps_u00']
-    assert features_u1['n_sps'] == 19.0
-    assert features_u1['n_static'] == 4080.0
-    assert features_u1['n_moving'] == 151.0
-    assert features_u1['n_rare'] == 171.0
-    assert features_u1['n_home'] == 2148.0
-    assert np.abs(features_u1['max_dist_home'] - 6392724.065) < 0.1
-    assert features_u1['n_transitions'] == 354.0
-    assert features_u1['n_top1'] == 2170.0
-    assert features_u1['n_top2'] == 1207.0
-    assert features_u1['n_top3'] == 171.0
-    assert features_u1['n_top4'] == 165.0
-    assert features_u1['n_top5'] == 63.0
-    assert np.abs(features_u1['entropy'] - 7.238) < 0.1
-    assert np.abs(features_u1['normalized_entropy'] - 2.458) < 0.1
-    assert np.abs(features_u1['dist_total'] - 18633281.869) < 0.1
-    assert features_u1['n_bins'] == 4231.0
-    assert np.abs(features_u1['speed_average'] - 0.287) < 0.1
-    assert np.abs(features_u1['speed_variance'] - 6.311) < 0.1
-    assert np.abs(features_u1['speed_max'] - 34.0) < 0.1
-    assert np.abs(features_u1['variance'] - 183.829) < 0.1
-    assert np.abs(features_u1['log_variance'] - 5.214) < 0.1
+
+    features_u1 = features[features['user'] == 'gps_u00']
+    assert features_u1['n_sps'][0] == 22.0
+    assert features_u1['n_static'][0] == 4095.0
+    assert features_u1['n_moving'][0] == 152.0
+    assert features_u1['n_rare'][0] == 114.0
+    assert features_u1['n_home'][0] == 2147.0
+    assert np.abs(features_u1['max_dist_home'][0] - 1041741.829247) < 0.1
+    assert features_u1['n_transitions'][0] == 370.0
+    assert features_u1['n_top1'][0] == 2155.0
+    assert features_u1['n_top2'][0] == 1291.0
+    assert features_u1['n_top3'][0] == 174.0
+    assert features_u1['n_top4'][0] == 114.0
+    assert features_u1['n_top5'][0] == 65.0
+    assert np.abs(features_u1['entropy'][0] - 7.193024) < 0.1
+    assert np.abs(features_u1['normalized_entropy'][0] - 2.327054) < 0.1
+    assert np.abs(features_u1['dist_total'][0] - 9806714.819275) < 0.1
+    assert features_u1['n_bins'][0] == 4247.0
+    assert np.abs(features_u1['speed_average'][0] - 0.289073) < 0.1
+    assert np.abs(features_u1['speed_variance'][0] - 6.34365) < 0.1
+    assert np.abs(features_u1['speed_max'][0] - 34.0) < 0.1
+    assert np.abs(features_u1['variance'][0] - 3.865936) < 0.1
+    assert np.abs(features_u1['log_variance'][0] - 1.352204) < 0.1
