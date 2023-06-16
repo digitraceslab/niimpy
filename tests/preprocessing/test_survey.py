5561e60c5eb572d0ecfae73b99c99aa17f81276d
Jarno Rantaharju <jarno.rantaharju@aalto.fi>
2023-06-16 19:43:37 +0300

    Update tests and add a test for test_convert_survey_to_numerical_answer

--- tests/preprocessing/test_survey.py
+++ tests/preprocessing/test_survey.py
@@ -16,12 +16,22 @@ df1 = df1.set_index('time')
 df2 = pd.DataFrame(
         {"time": ['2019-01-01'] + ['2019-01-02'] + ['2019-01-03'],
          "user": [1, 1, 3],
-         "S1_1": ["no", "some", "a_lot"],
-         "S1_2": ["no", "some", "a_lot"],
+         "S1_1": ["no", "some", "a lot"],
+         "S1_2": ["no", "some", "a lot"],
          "S1_3": ["no", "some", ""],
         })
 df2['time'] = pd.to_datetime(df2['time'])
 df2 = df2.set_index('time')
+
+df3 = pd.DataFrame(
+        {"time": ['2019-01-01'] + ['2019-01-02'] + ['2019-01-03'],
+         "user": [1, 1, 3],
+         "question_1.": ["no", "some", "a lot"],
+         "question_2,": ["no", "some", "a lot"],
+         "question_3?": ["no", "some", ""],
+        })
+df3['time'] = pd.to_datetime(df3['time'])
+df3 = df3.set_index('time')
 
 def test_sum_survey_scores():
     df = df1.copy()
@@ -38,7 +48,7 @@ def test_convert_survey_to_numerical_answer():
     ID_MAP = {"S1": {
         "no": 0,
         "some": 1,
-        "a_lot": 2,
+        "a lot": 2,
     }}
     results = niimpy.preprocessing.survey.convert_survey_to_numerical_answer(
         df,
@@ -50,5 +60,14 @@ def test_convert_survey_to_numerical_answer():
     assert results.loc['2019-01-02']['S1_2'] == 1
     assert results.loc['2019-01-03']['S1_1'] == 2
     assert np.isnan(results.loc['2019-01-03']['S1_3'])
+
+
+def test_clean_survey_column_names():
+    df = df3.copy()
+
+    cleaned_df = niimpy.preprocessing.survey.clean_survey_column_names(df)
+
+    expected_column_names = ["user", 'question 1', 'question 2', 'question 3']
+    assert all([a == b for a, b in zip(cleaned_df.columns, expected_column_names)])
 
 
