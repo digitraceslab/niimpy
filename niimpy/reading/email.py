ee892c5d409a2d748b4832180240ca91fd9b8be5
Rantaharju Jarno <jarno.rantaharju@aalto.fi>
2023-12-12 12:49:25 +0200

    Turn MailboxReader into a context manager and add a property messages that is the generator

--- niimpy/reading/email.py
+++ niimpy/reading/email.py
@@ -1,29 +1,35 @@
-from zipfile import ZipFile
 import email
 
-class MailboxREader():
+class MailboxReader():
     def __init__(self, file_handle):
         if type(file_handle) == str:
             self.file = open(file_handle)
         else:
             self.file = file_handle
 
-        self.lines = []
-        self.is_done = False
-
-    def __next__(self):
-        while not self.is_done:
+    @property
+    def messages(self):
+        lines = []
+        while True:
             line = self.file.readline()
-            if line == b'' or line.startswith(b'From '):
-                if line == b'':
-                    self.is_done = True
-                if self.lines:
-                    message = email.message_from_bytes(b''.join(self.lines))
-                    self.lines = [line]
-                    return message
             
-            self.lines.append(line)
+            if line.startswith(b'From ') or line == b'':
+                if lines:
+                    message = email.message_from_bytes(b''.join(lines))
+                    yield(message)
+                
+                if line == b'':
+                    break
+                
+                lines = [line]
+
+            lines.append(line)
 
     def close(self):
         self.file.close()
 
+    def __enter__(self):
+        return self
+    
+    def __exit__(self, type, value, traceback):
+        self.close()
