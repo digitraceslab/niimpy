33d8710ea5f1956455c658386cab9fa8f300dac3
Your Name <luongnguyenkhoinguyen14@gmail.com>
2024-02-27 13:10:58 +0200

    Lint app file and test

--- tests/preprocessing/test_apps.py
+++ tests/preprocessing/test_apps.py
@@ -4,33 +4,40 @@ from niimpy import config
 import pytest
 
 # read sample data
-data = niimpy.read_csv(config.SINGLEUSER_AWARE_APP_PATH, tz='Europe/Helsinki')
-screen = niimpy.read_csv(config.MULTIUSER_AWARE_SCREEN_PATH, tz='Europe/Helsinki')
-battery = niimpy.read_csv(config.MULTIUSER_AWARE_BATTERY_PATH, tz='Europe/Helsinki')
+data = niimpy.read_csv(config.SINGLEUSER_AWARE_APP_PATH, tz="Europe/Helsinki")
+screen = niimpy.read_csv(config.MULTIUSER_AWARE_SCREEN_PATH, tz="Europe/Helsinki")
+battery = niimpy.read_csv(config.MULTIUSER_AWARE_BATTERY_PATH, tz="Europe/Helsinki")
+
 
 def test_app_features():
-    
-    # TEST 1 
-    test = app.extract_features_app(data, battery, screen, features=None)    
-    
+    # TEST 1
+    test = app.extract_features_app(data, battery, screen, features=None)
+
     user_comm = test[(test["user"] == "iGyXetHE3S8u") & (test["app_group"] == "comm")]
     user_work = test[(test["user"] == "iGyXetHE3S8u") & (test["app_group"] == "work")]
-    assert user_comm.loc['2019-08-05 14:00:00+03:00']["count"] == 2
-    assert user_work.loc['2019-08-06 04:00:00+03:00']["count"] == 1
-    assert user_comm.loc['2019-08-05 14:00:00+03:00']["duration"] == pytest.approx(594.99) 
-    assert user_work.loc['2019-08-06 04:00:00+03:00']["duration"] == pytest.approx(778.0) 
-    
+    assert user_comm.loc["2019-08-05 14:00:00+03:00"]["count"] == 2
+    assert user_work.loc["2019-08-06 04:00:00+03:00"]["count"] == 1
+    assert user_comm.loc["2019-08-05 14:00:00+03:00"]["duration"] == pytest.approx(
+        594.99
+    )
+    assert user_work.loc["2019-08-06 04:00:00+03:00"]["duration"] == pytest.approx(
+        778.0
+    )
+
     # TEST 2
-    features ={app.app_count:{"app_column_name":"application_name", 
-                              "screen_column_name":"screen_status",
-                              "resample_args":{"rule":"2H"}},
-               app.app_duration:{"resample_args":{"rule":"1H"}}}
+    features = {
+        app.app_count: {
+            "app_column_name": "application_name",
+            "screen_column_name": "screen_status",
+            "resample_args": {"rule": "2H"},
+        },
+        app.app_duration: {"resample_args": {"rule": "1H"}},
+    }
     test = app.extract_features_app(data, battery, screen, features=features)
     user_comm = test[(test["user"] == "iGyXetHE3S8u") & (test["app_group"] == "comm")]
     user_work = test[(test["user"] == "iGyXetHE3S8u") & (test["app_group"] == "work")]
 
-    assert user_comm.loc['2019-08-05 20:00:00+03:00']["count"] == 3
-    assert user_work.loc['2019-08-06 04:00:00+03:00']["count"] == 2
-    assert user_comm.loc['2019-08-05 20:00:00+03:00']["duration"] == 3569.00
-    assert user_work.loc['2019-08-06 04:00:00+03:00']["duration"] == 2578
-       
+    assert user_comm.loc["2019-08-05 20:00:00+03:00"]["count"] == 3
+    assert user_work.loc["2019-08-06 04:00:00+03:00"]["count"] == 2
+    assert user_comm.loc["2019-08-05 20:00:00+03:00"]["duration"] == 3569.00
+    assert user_work.loc["2019-08-06 04:00:00+03:00"]["duration"] == 2578
